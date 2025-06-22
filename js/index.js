88293567a8c830cd96064959030894cff97b27d2
aglitchinreality1 <119508336+aglitchinreality1@users.noreply.github.com>
2025-06-22 17:27:15 +0530

    test3

--- js/index.js
+++ js/index.js
@@ -244,64 +244,64 @@ document.getElementById("go").addEventListener("click", () => {
 });
 
 
-// function setupBidirectionalAutoScroll(className) {
-//   const elements = document.querySelectorAll(`.${className}`);
+function setupBidirectionalAutoScroll(className) {
+  const elements = document.querySelectorAll(`.${className}`);
 
-//   elements.forEach(el => {
-//     let autoScrollActive = true;
-//     let scrollTimeout;
-//     let userInteracting = false;
+  elements.forEach(el => {
+    let autoScrollActive = true;
+    let scrollTimeout;
+    let userInteracting = false;
 
-//     const startAutoScroll = () => {
-//       autoScrollActive = true;
-//       let scrollingDown = true;
+    const startAutoScroll = () => {
+      autoScrollActive = true;
+      let scrollingDown = true;
 
-//       const scrollLoop = async () => {
-//         while (autoScrollActive) {
-//           if (scrollingDown) {
-//             if (el.scrollTop + el.clientHeight < el.scrollHeight - 1) {
-//               el.scrollTop += 1;
-//             } else {
-//               scrollingDown = false;
-//               await new Promise(res => setTimeout(res, 500));
-//             }
-//           } else {
-//             if (el.scrollTop > 0) {
-//               el.scrollTop -= 1;
-//             } else {
-//               scrollingDown = true;
-//               await new Promise(res => setTimeout(res, 500));
-//             }
-//           }
-//           await new Promise(res => setTimeout(res, 20));
-//         }
-//       };
+      const scrollLoop = async () => {
+        while (autoScrollActive) {
+          if (scrollingDown) {
+            if (el.scrollTop + el.clientHeight < el.scrollHeight - 1) {
+              el.scrollTop += 1;
+            } else {
+              scrollingDown = false;
+              await new Promise(res => setTimeout(res, 500));
+            }
+          } else {
+            if (el.scrollTop > 0) {
+              el.scrollTop -= 1;
+            } else {
+              scrollingDown = true;
+              await new Promise(res => setTimeout(res, 500));
+            }
+          }
+          await new Promise(res => setTimeout(res, 20));
+        }
+      };
 
-//       scrollLoop();
-//     };
+      scrollLoop();
+    };
 
-//     const stopAutoScroll = () => {
-//       autoScrollActive = false;
-//       clearTimeout(scrollTimeout);
-//       scrollTimeout = setTimeout(() => {
-//         startAutoScroll();
-//       }, 1000); 
-//     };
+    const stopAutoScroll = () => {
+      autoScrollActive = false;
+      clearTimeout(scrollTimeout);
+      scrollTimeout = setTimeout(() => {
+        startAutoScroll();
+      }, 1000); 
+    };
 
-//     el.addEventListener('scroll', () => {
-//       if (!userInteracting) {
-//         userInteracting = true;
-//         stopAutoScroll();
-//       }
-//     });
+    el.addEventListener('scroll', () => {
+      if (!userInteracting) {
+        userInteracting = true;
+        stopAutoScroll();
+      }
+    });
 
-//     startAutoScroll();
-//   });
-// }
+    startAutoScroll();
+  });
+}
 
-// setupBidirectionalAutoScroll('his');
-// setupBidirectionalAutoScroll('resume');
-// setupBidirectionalAutoScroll('skill-list');
+setupBidirectionalAutoScroll('his');
+setupBidirectionalAutoScroll('resume');
+setupBidirectionalAutoScroll('skill-list');
 
 function setupHorizontalAutoScroll(className) {
   const containers = document.querySelectorAll(`.${className}`);
@@ -355,9 +355,3 @@ setupHorizontalAutoScroll('achcontent');
 
 setupHorizontalAutoScroll('expcont');
 
-setInterval(() => {
-  const button = document.getElementById('pright');
-  if (button) {
-    button.click();
-  }
-}, 7000);
